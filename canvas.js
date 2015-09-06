232e003f95adb64a44e8525b8e5709dc0877eca3
GusChen <guanxun_chen@foxmail.com>
2015-09-06 11:10:07 +0800

    添加选项

--- canvas.js
+++ canvas.js
@@ -1,9 +1,9 @@
 (function () {
-	var config = {},
-		cvs,
-		cxt,
-		lastX =0,
-		lastY=0,
+	var config = {}, //基础配置
+		cvs, //dom对象
+		cxt, //canvas对象
+		lastX ,//起始位置x
+		lastY ,//起始位置y
 		mousePressed = false,
 		canvas = function (obj) {
 			init(obj);
@@ -41,14 +41,24 @@
 			mousePressed = false;
 		});
 		//draw(100, 100, true)
+		document.getElementById("clearDraw").addEventListener("click",function (){
+			clear_draw();
+		},false);
+		setLineColor();
+		setLineWidth();
 	}
 	//颜色
 	function setLineColor() {
-
+		document.getElementById("DrawColor").addEventListener("change",function (e){
+			config.linecolor = this.value;
+		},false);
 	}
 	//线条宽度
 	function setLineWidth() {
-
+		document.getElementById("DrawWidth").addEventListener("change",function (e){
+			config.linewidth = this.value;
+		},false);
+		//
 	}
 	//重置画板
 	function clear_draw() {
@@ -57,40 +67,48 @@
 	//绘制
 	function draw(x, y, isDraw) {
 		if (isDraw) {
+			//绘制开始
 			cxt.beginPath();
+			//颜色
 			cxt.strokeStyle = config.linecolor;
+			//宽度
 			cxt.lineWidth = config.linewidth;
+			//线条
 			cxt.lineJoin = "round";
-			
+			//起始位置
 			cxt.moveTo(lastX, lastY);
+			//结束位置
 			cxt.lineTo(x, y);
-			console.log(lastX);
-			console.log(lastY);
-			console.log(x);
-			console.log(y);
+			//结束配置
 			cxt.closePath();
+			//绘制
 			cxt.stroke();
 		}
+		//将上一次的结束位置作为新的开始
 		lastX = x;
 		lastY = y;
 	}
 
+	//当前元素相对于浏览器的位置
 	function getOffsetRect(ele){
-      var box=ele.getBoundingClientRect();
-      var body=document.body,
-        docElem=document.documentElement;
-      //获取页面的scrollTop,scrollLeft(兼容性写法)
-      var scrollTop=window.pageYOffset||docElem.scrollTop||body.scrollTop,
-        scrollLeft=window.pageXOffset||docElem.scrollLeft||body.scrollLeft;
-      var clientTop=docElem.clientTop||body.clientTop,
-        clientLeft=docElem.clientLeft||body.clientLeft;
-      var top=box.top+scrollTop-clientTop,
-        left=box.left+scrollLeft-clientLeft;
-      return {
-        //Math.round 兼容火狐浏览器bug
-        top:Math.round(top),
-        left:Math.round(left)
-      }
+    	var box=ele.getBoundingClientRect();
+    	var body=document.body,
+    		docElem=document.documentElement;
+  		//获取页面的scrollTop,scrollLeft(兼容性写法)
+  		var scrollTop=window.pageYOffset||docElem.scrollTop||body.scrollTop,
+    		scrollLeft=window.pageXOffset||docElem.scrollLeft||body.scrollLeft;
+
+  		var clientTop=docElem.clientTop||body.clientTop,
+    		clientLeft=docElem.clientLeft||body.clientLeft;
+
+  		var top=box.top+scrollTop-clientTop,
+    		left=box.left+scrollLeft-clientLeft;
+
+  		return {
+    		//Math.round 兼容火狐浏览器bug
+    		top:Math.round(top),
+    		left:Math.round(left)
+  		}
     }
 	//将canvas对象连接到window上
 	window.canvas = canvas;
